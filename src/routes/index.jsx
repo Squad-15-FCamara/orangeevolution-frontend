9e32570905b97cb556a2fceb3f6e06a9fe3582ef
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-08 14:12:56 -0300

    feat: create global styles

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyles } from '../styles/GlobalStyles';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   return (
     <BrowserRouter>
+      <GlobalStyles/>
       <AppRoutes />
     </BrowserRouter>
   );
