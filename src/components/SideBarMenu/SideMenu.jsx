9d88d470bb6dd6ba8f2e6749825ee7b22c7ffbb6
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-09 12:48:56 -0300

    feat: created desktop sidebar menu

---
+++ src/components/SideBarMenu/SideMenu.jsx
@@ -0,0 +1,63 @@
+import { faArrowRightFromBracket, faChartLine, faBell, faFolderOpen, faStairs, faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import avatar from './avatar.svg'
+import {
+  Wrapper,
+  Logo,
+  Menu,
+  Links,
+  LinkStyled,
+  SearchIcon,
+  Input,
+  HomeIcon,
+  Text,
+  TrilhasIcon,
+  PastasIcon,
+  NotificaIcon,
+  ProgressoIcon,
+  Profile,
+  Avatar,
+  Name,
+  LogOutIcon } from "./style";
+
+export const SideMenu = () => {
+  return(
+    <Wrapper>
+    <Logo/>
+      <Menu>
+
+          <Links>
+            <LinkStyled>
+              <SearchIcon icon={faMagnifyingGlass} />
+              <Input placeholder='Buscar'/>
+            </LinkStyled>
+            <LinkStyled href='/'>
+              <HomeIcon icon={faHouse}/>
+              <Text>Home</Text>
+            </LinkStyled>
+            <LinkStyled href='/roads'>
+              <TrilhasIcon icon={faStairs}/>
+              <Text>Trilhas</Text>
+            </LinkStyled>
+            <LinkStyled href='/folders'>
+              <PastasIcon icon={faFolderOpen}/>
+              <Text>Pastas</Text>
+            </LinkStyled>
+            <LinkStyled href='/profile'>
+              <NotificaIcon icon={faBell} />
+              <Text>Notificações</Text>
+            </LinkStyled>
+            <LinkStyled href='/profile'>
+              <ProgressoIcon icon={faChartLine}/>
+              <Text>Progresso</Text>
+            </LinkStyled>
+          </Links>
+        <Profile>
+          <Avatar src={avatar}/>
+          <Name>Thiago Bandeira</Name>
+          <LogOutIcon icon={faArrowRightFromBracket}/>
+        </Profile>
+
+      </Menu>
+    </Wrapper>
+  )
+}
---
+++ src/components/SideBarMenu/style.js
@@ -0,0 +1,163 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export const Wrapper = styled.div`
+   width: 235px;
+  height: 100vh;
+`
+
+export const Menu = styled.div`
+  width: 235px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+`
+
+export const Logo = styled.img`
+  height: 80px;
+  width: 223px;
+  border: 1px solid #36357E;
+
+`
+
+export const LinkStyled = styled.a`
+  display: flex;
+  height: 48px;
+  align-items: center;
+  text-decoration: none;
+  gap: 10px;
+  width: 219px;
+  border-radius: 10px;
+  margin-top: 16px;
+
+  :hover{
+    background-color: #36357E;
+    color: white;
+    cursor: pointer;
+
+    >*{
+    color: white;
+    background-color: #36357E;
+    }
+  }
+`
+export const Links = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const Input = styled.input`
+  height: 25px;
+  border: none;
+  outline: none;
+  font-size: 20px;
+  padding: 2px;
+  max-width: 130px;
+  font-family: 'Montserrat';
+  background-color: transparent;
+
+  ::placeholder{
+    color: #36357E;
+    font-weight: 400;
+  }
+
+  :hover {
+
+    ::placeholder{
+      color: white;
+    }
+  }
+`
+
+export const Text = styled.p`
+  color: #36357E;
+  list-style-type: none;
+  font-size: 20px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+
+  :hover{
+    background-color: #36357E;
+    color: white;
+  }
+`
+
+export const SearchIcon = styled(FontAwesomeIcon)`
+    color: #36357E;
+    padding-left: 29px;
+    font-size: 25px;
+
+`
+
+export const TrilhasIcon = styled(FontAwesomeIcon)`
+  color: #36357E;
+  padding-left: 29px;
+  font-size: 25px;
+
+`
+export const HomeIcon = styled(FontAwesomeIcon)`
+  color: #36357E;
+  padding-left: 29px;
+  font-size: 25px;
+
+`
+
+export const PastasIcon = styled(FontAwesomeIcon)`
+  color: #36357E;
+  padding-left: 29px;
+  font-size: 25px;
+
+`
+
+export const NotificaIcon = styled(FontAwesomeIcon)`
+  color: #36357E;
+  padding-left: 29px;
+  font-size: 25px;
+
+
+`
+
+export const ProgressoIcon = styled(FontAwesomeIcon)`
+  color: #36357E;
+  padding-left: 29px;
+  font-size: 25px;
+
+`
+
+export const LogOutIcon = styled(FontAwesomeIcon)`
+  color: #ffffff;
+  transform: rotate(180deg);
+  font-size: 25px;
+
+
+`
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #36357E;
+  gap: 10px;
+  height: 80px;
+  border-radius: 10px 10px 0px 0px;
+  position: absolute;
+  bottom: 0;
+  width: 235px;
+`
+
+export const Avatar = styled.img`
+   max-width: 40px;
+   padding-left: 16px;
+`
+
+
+export const Name = styled.p`
+  color: #FBFBFB;
+  font-size: 14px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+`
