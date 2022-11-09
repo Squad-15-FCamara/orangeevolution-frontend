1e464fb89e66e50d5158f7d1626149573b28b011
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-09 14:07:56 -0300

    feat: add SideMenu to default layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 // import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { SideMenu } from '../components/SideBarMenu/SideMenu';
 
 export function DefaultLayout() {
   return (
     <div>
       {/* <Header /> */}
+      <SideMenu/>
       <Outlet />
       <Footer />
     </div>
