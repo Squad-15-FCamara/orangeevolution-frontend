fabb52ac7edae442fce49c6c5355df8aed78719a
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-09 02:38:08 -0300

    feat: changes component to be imported completed

--- src/components/RoadCard/index.jsx
+++ src/components/RoadCard/index.jsx
@@ -1,5 +1,21 @@
-import { Button } from './style';
+import { Link } from 'react-router-dom';
+import { Button, Wrapper } from './style';
 
 export function RoadCard() {
-  return <Button></Button>;
+  return (
+    <Wrapper>
+      <Link to={'/roaddev/'}>
+        <Button imgRoad="url(./cardDev.svg)"></Button>
+      </Link>
+      <Link to={'/roadux/'}>
+        <Button imgRoad="url(./cardUx.svg)"></Button>
+      </Link>
+      <Link to={'/roadqa/'}>
+        <Button imgRoad="url(./cardQa.svg)"></Button>
+      </Link>
+      <Link to={'/roadsoft/'}>
+        <Button imgRoad="url(./cardSoft.svg)"></Button>
+      </Link>
+    </Wrapper>
+  );
 }
--- src/components/RoadCard/style.js
+++ src/components/RoadCard/style.js
@@ -7,3 +7,10 @@ export const Button = styled.button`
   border: none;
   background-image: ${(props) => props.imgRoad};
 `;
+
+export const Wrapper = styled.div`
+  width: 700px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px 32px;
+`;
