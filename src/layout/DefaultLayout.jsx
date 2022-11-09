12c41e76fbdff53dbccca2be0850fbc3e5866c74
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-09 00:35:14 -0300

    feat: adds footer as a default layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 // import { Header } from '../components/Header';
-// import { Footer } from '../components/Footer';
+import { Footer } from '../components/Footer';
 
 export function DefaultLayout() {
   return (
     <div>
       {/* <Header /> */}
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
