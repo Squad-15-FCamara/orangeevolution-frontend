abcd1ad2b6605df565ba69298dccd38c09f5ecfe
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-09 20:25:05 -0300

    feat: add carousel cards to homepage

---
+++ src/pages/Home/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+export const Card = styled.img`
+  object-fit: cover;
+`
+
+export const Header = styled.h1`
+  font-size: 48px;
+  font-weight: 700;
+`
+
+export const SubHeader = styled.h3`
+  font-size: 32px;
+  font-weight: 500;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100%;
+  margin: 0 auto;
+  margin-bottom: 200px;
+  max-width: 800px;
+  width: 100%;
+`
+
+export const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 300px;
+  width: 100%;
+
+`
+
