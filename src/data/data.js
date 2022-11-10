6527348d62fb3b77a36285110ba6c088cd6e3419
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-10 00:58:42 -0300

    docs: rewrite data that will be used to put contents on db

--- src/data/data.js
+++ src/data/data.js
@@ -1,129 +1,211 @@
 export const data = {
-  "courses":[
+  courses: [
+    {
+      title: 'Qual a melhor linguagem de programação? | Com Willian da Silva',
+      road: 'FullStack',
+      type: 'Video',
+      theme: 'Introdução',
+      author: 'Orange Juice',
+      time: '51',
+      tags: 'linguagem, programacao, codigo',
+      link: 'https://www.youtube.com/watch?v=zTMvQD5EtJw',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-            {
-            "title": "Qual melhor linguagem de programação? | Com Wilian da Silva",
-            "road": "dev",
-            "type": "vídeo",
-            "theme": "programação",
-            "author": "OrangeJuice",
-            "time": "51",
-            "tags": "linguagem,programação, código",
-            "link":"https://www.youtube.com/watch?v=zTMvQD5EtJw",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Habilidades além do código! | Com Mateus Oliveira',
+      road: 'FullStack',
+      type: 'Video',
+      theme: 'Introdução',
+      author: 'Orange Juice',
+      time: '45',
+      tags: 'softskill, comunicacao, habilidades',
+      link: 'https://www.youtube.com/watch?v=Mmukepu3yRs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
+    {
+      title: 'Lógica de programação e algoritmos',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Lógica de Programação',
+      author: 'Udemy',
+      time: '120',
+      tags: 'logica, algoritmos, curso',
+      link: 'https://www.udemy.com/course/logica-de-programacao-e-algoritmos-iniciante/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-            {
-            "title": "Habilidades além do código! | Com Mateus Oliveira",
-            "road": "dev",
-            "type": "vídeo",
-            "theme": "programação",
-            "author": "OrangeJuice",
-            "time": "45",
-            "tags": "softskill, comunicação, habilidades",
-            "link":"https://www.youtube.com/watch?v=Mmukepu3yRs",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Instalar o Linux no Windows com o WSL',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Sistemas Operacionais',
+      author: 'Windows',
+      time: '10',
+      tags: 'linux, windows, so',
+      link: 'https://docs.microsoft.com/pt-br/windows/wsl/install',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
+    {
+      title: 'Terminal Linux',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Sistemas Operacionais',
+      author: 'Udemy',
+      time: '55',
+      tags: 'linux, so',
+      link: 'https://www.udemy.com/course/terminal-de-comandos-linux/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
+    {
+      title: 'Versionamento com Git',
+      road: 'FullStack',
+      type: 'Video',
+      theme: 'Introdução ao Git',
+      author: 'Orange Juice',
+      time: '63',
+      tags: 'git, github',
+      link: 'https://www.youtube.com/watch?v=9k_lIYuRtg8',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-            {
-            "title": "Lógica de programação e algoritmos",
-            "road": "dev",
-            "type": "curso",
-            "theme": "programação",
-            "author": "Udemy",
-            "time": "51",
-            "tags": "logica, algoritmos, curso",
-            "link":"https://www.udemy.com/course/logica-de-programacao-e-algoritmos-iniciante/",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Git e Github para iniciantes',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Introdução ao Git',
+      author: 'Udemy',
+      time: '125',
+      tags: 'git, github',
+      link: 'https://www.udemy.com/course/git-e-github-para-iniciantes/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
+    {
+      title: 'Introdução a Bancos de Dados e Linguagem SQL',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Banco de dados',
+      author: 'Udemy',
+      time: '119',
+      tags: 'sql, db, dados',
+      link: 'https://www.udemy.com/course/introducao-a-bancos-de-dados-e-linguagem-sql/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-            {
-            "title": "Básico HTML, CSS e Javascript",
-            "road": "dev",
-            "type": "curso",
-            "theme": "frontend",
-            "author": "Udemy",
-            "time": "131",
-            "tags": "HTML, CSS, JavaScript",
-            "link":"https://www.udemy.com/course/criando-site-html-css-e-javascript/",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Básico HTML, CSS e Javascript',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Introdução ao FrontEnd',
+      author: 'Udemy',
+      time: '131',
+      tags: 'HTML, CSS, JavaScript',
+      link: 'https://www.udemy.com/course/criando-site-html-css-e-javascript/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-             {
-            "title": "HTTP: Desmistificando o protocolo da Web",
-            "road": "dev",
-            "type": "artigo",
-            "theme": "frontend",
-            "author": "Alura",
-            "time": "45",
-            "tags": "HTTP, internet, Web",
-            "link":"https://www.alura.com.br/artigos/desmistificando-o-protocolo-http-parte-1",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Introdução à linguagem CSS',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Introdução ao FrontEnd',
+      author: 'Udemy',
+      time: '87',
+      tags: 'css, frontend, Web',
+      link: 'https://www.udemy.com/course/introducao-a-linguagem-css/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
+    {
+      title: 'Curso de Sass',
+      road: 'FullStack',
+      type: 'Curso',
+      theme: 'Introdução ao FrontEnd',
+      author: 'Amanda Vilela',
+      time: '106',
+      tags: 'css, frontend, Web',
+      link: 'https://www.youtube.com/playlist?list=PL97KElaimHeGRtfkksKwxg6IGVZi_cR7J',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-              {
-            "title": "O que é uma API?",
-            "road": "dev",
-            "type": "artigo",
-            "theme": "backend",
-            "author": "AWS",
-            "time": "180",
-            "tags": "API, REST, Cloud",
-            "link":"https://aws.amazon.com/pt/what-is/api/",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Descomplicando SPA',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Introdução ao FrontEnd',
+      author: 'Training Center',
+      time: '15',
+      tags: 'spa, frontend, Web',
+      link: 'https://medium.com/trainingcenter/descomplicando-spas-caa8f57bdbf3',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-              {
-            "title": "Introdução a Bancos de Dados e Linguagem SQL",
-            "road": "dev",
-            "type": "curso",
-            "theme": "backend",
-            "author": "Udemy",
-            "time": "120",
-            "tags": "SQL, banco, dados",
-            "link":"https://www.udemy.com/course/introducao-a-bancos-de-dados-e-linguagem-sql/",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'HTTP: Desmistificando o protocolo da Web',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Introdução a Redes',
+      author: 'Alura',
+      time: '10',
+      tags: 'redes, http, Web',
+      link: 'https://www.alura.com.br/artigos/desmistificando-o-protocolo-http-parte-1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-               {
-            "title": "Instalar o Linux no Windows com o WSL",
-            "road": "dev",
-            "type": "artigo",
-            "theme": "backend",
-            "author": "Windows",
-            "tempo": "20",
-            "tags": "Linux, WSL, Windows",
-            "link":"https://learn.microsoft.com/pt-br/windows/wsl/install",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
+    {
+      title: 'Qual é a diferença entre HTTP e HTTPS?',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Introdução a Redes',
+      author: 'Alura',
+      time: '15',
+      tags: 'redes, http, Web',
+      link: 'https://www.alura.com.br/artigos/qual-e-diferenca-entre-http-e-https',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
 
-            {
-            "title": "Versionamento com Git",
-            "road": "dev",
-            "type": "video",
-            "theme": "frontend",
-            "author": "OrangeJuice",
-            "time": "63",
-            "tags": "Gitcódigo, GitFlow",
-            "link":"https://www.youtube.com/watch?v=9k_lIYuRtg8",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              },
-            {
-            "title": "Git e Github para iniciantes",
-            "road": "dev",
-            "type": "curso",
-            "theme": "frontend",
-            "author": "Udemy",
-            "tempo": "125",
-            "tags": "Git, código, GitFlow",
-            "status": "unresolved",
-            "link":"https://www.udemy.com/course/git-e-github-para-iniciantes/",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
-              }
-           ]
-}
+    {
+      title: 'Métodos de requisição do HTTP',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Introdução a Redes',
+      author: 'Alura',
+      time: '10',
+      tags: 'redes, http, Web',
+      link: 'https://www.alura.com.br/artigos/metodos-de-requisicao-do-http',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
+
+    {
+      title: 'O que é uma API?',
+      road: 'FullStack',
+      type: 'Artigo',
+      theme: 'Introdução ao BackEnd',
+      author: 'AWS',
+      time: '180',
+      tags: 'API, REST, Cloud',
+      link: 'https://aws.amazon.com/pt/what-is/api/',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam.',
+    },
+  ],
+};
