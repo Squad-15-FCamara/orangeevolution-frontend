36043d83a4a2c2bfacec03e76f8dccfb363e9ddd
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-12 01:00:45 -0300

    feat: finish fullstack content page

--- src/pages/ContentFull/index.jsx
+++ src/pages/ContentFull/index.jsx
@@ -1,7 +1,57 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+
+import { Wrapper } from '../Home/style';
+import { Gradient, LinksContainer, Stats, Themes } from '../ContentIntro/style';
+import { ChosedRoadCard } from '../../components/ChosedRoadCard';
+import { ContainerTheme } from '../RoadDev/style';
+
 export function ContentFull() {
+  const [roadDev, setRoadDev] = useState([]);
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
+  async function getCourses() {
+    await api
+      .get('/courses/themes/fullstack')
+      .then((response) => setRoadDev(response.data))
+      .catch((err) => {
+        console.error('ops! ocorreu um erro' + err);
+      });
+    [];
+  }
+
   return (
-    <div>
-      <h1>Tela de conteúdos da trilha Full Stack</h1>
-    </div>
+    <Wrapper>
+      <LinksContainer>
+        <a href="/">Home</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="/roaddev">Dev Full Stack</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="#">Full Stack</a>
+      </LinksContainer>
+      <Gradient>Full Stack</Gradient>
+      <Stats>xxx pessoas estão estudando essa trilha</Stats>
+      <Themes>Conteúdos - 00/30</Themes>
+      <ContainerTheme>
+        {roadDev.map((card, index) => (
+          <ChosedRoadCard
+            id={card.id}
+            title={card.title}
+            idType={card.idType}
+            time={card.time}
+            idTheme={card.idTheme}
+            idRoad={card.idRoad}
+            link={card.link}
+            key={index}
+          />
+        ))}
+      </ContainerTheme>
+    </Wrapper>
   );
 }
