caf3d968f307c6d05edb95caa46b094df5fdd1fa
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-13 13:02:02 -0300

    fix: style to change buttonS dinamically

--- src/pages/SavedContent/style.js
+++ src/pages/SavedContent/style.js
@@ -21,7 +21,6 @@ export const ButtonS = styled.button`
   display: flex;
   align-items: center;
   font-size: 1.75rem;
-  font-weight: ${(props) => (props.isclicked === false ? 500 : 700)};
-  text-decoration: ${(props) =>
-    props.isclicked === false ? 'none' : 'underline'};
+  font-weight: ${(props) => !props.clicked ? 500 : 700};
+  text-decoration: ${(props) => !props.clicked ? 'none' : 'underline'};
 `;
