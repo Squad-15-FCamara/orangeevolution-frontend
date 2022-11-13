3bddfd0e83ce3500e0ee4c3d25a9ebe7665aec57
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-12 22:54:45 -0300

    feat: page structure

--- src/pages/SavedContent/index.jsx
+++ src/pages/SavedContent/index.jsx
@@ -1,4 +1,10 @@
 import { useState, useEffect } from 'react';
+import { ChosedRoadCard } from '../../components/ChosedRoadCard';
+import { ContainerTheme } from '../ContentIntro/style';
+import { Wrapper } from '../Home/style';
+import { Stats } from '../RoadDev/style';
+import { ButtonS, FilterContainer, PageTitle } from './style';
+import { statisticsService } from '../../services/statisticsService';
 
 export function SavedContent() {
   const [filtered, setFiltered] = useState([]);
@@ -19,4 +25,27 @@ export function SavedContent() {
     setFiltered(filteredContent);
   }
 
+  return (
+    <Wrapper>
+      <PageTitle>Salvos</PageTitle>
+      <Stats>Acesse aqui seus conteúdos salvos</Stats>
+      <FilterContainer>
+        {favThemes ? favThemes.map((item, index) => <ButtonS key={index} /*clicked={isclicked}*/ onClick={() => fetchCourses(item)} >{item}</ButtonS>) : <h1>non ecsiste</h1>}
+      </FilterContainer>
+      <ContainerTheme>
+        {filtered ? filtered.map((card, index) => (
+          <ChosedRoadCard
+            id={card.id}
+            title={card.title}
+            idType={card.idType}
+            time={card.time}
+            idTheme={card.idTheme}
+            idRoad={card.idRoad}
+            link={card.link}
+            key={index}
+          />
+        )) : <h1>foi mal brother.. sem conteúdo</h1>}
+      </ContainerTheme>
+    </Wrapper>
+  );
 }
