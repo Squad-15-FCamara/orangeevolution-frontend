f75b39326894ad85c18061752d6bf173afb04cd4
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-12 22:09:31 -0300

    feat: create search page

---
+++ src/pages/Search/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+
+
+export const Wrapper = styled.div`
+  margin-left: 300px;
+  padding-top: 50px;
+  padding-bottom: 100px;
+`
+
+export const Title = styled.h1`
+  padding-bottom: 50px;
+  font-weight: 800;
+  font-size: 40px;
+`
