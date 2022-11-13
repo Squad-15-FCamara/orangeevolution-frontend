a594c88c2ecc2737463e6059b454dddd44be0858
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 20:58:47 -0300

    feat: creates adm homepage

---
+++ src/pages/HomeAdm/style.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Card = styled.img`
+  object-fit: cover;
+`;
+
+export const Header = styled.h1`
+  font-size: 3rem;
+  font-weight: 700;
+`;
+
+export const SubHeader = styled.h3`
+  font-size: 2rem;
+  font-weight: 500;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100%;
+  padding-left: 385px;
+  padding-bottom: 200px;
+  max-width: 1205px;
+  width: 100%;
+`;
+
+export const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 300px;
+  width: 75%;
+`;
+
+export const Status = styled.span`
+  font-size: 1.25rem;
+  color: #333333;
+  font-weight: 500;
+  margin-bottom: 16px;
+`;
+
+export const CardsStudy = styled(Cards)`
+  height: 360px;
+`;
