7817252b38da20a9c1faa2d585d48d3127a8a445
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-12 22:52:33 -0300

    feat: fetch favorite courses and favorite themes

--- src/pages/SavedContent/index.jsx
+++ src/pages/SavedContent/index.jsx
@@ -1,62 +1,13 @@
-import api from '../../services/api';
 import { useState, useEffect } from 'react';
-import { ChosedRoadCard } from '../../components/ChosedRoadCard';
-import { ContainerTheme } from '../ContentIntro/style';
-import { Wrapper } from '../Home/style';
-import { Stats } from '../RoadDev/style';
-import { ButtonS, FilterContainer, PageTitle } from './style';
 
 export function SavedContent() {
-  const [roadDev, setRoadDev] = useState([]);
-  const [isclicked, setIsClicked] = useState(false);
+  const [filtered, setFiltered] = useState([]);
+  const [favThemes, setFavThemes] = useState([]);
 
-  useEffect(() => {
-    api
-      .get('/statistics/favorites/courses/4')
-      .then((response) => setRoadDev(response.data))
-      .catch((err) => {
-        console.error('ops! ocorreu um erro' + err);
-      });
-  }, []);
-  function isClicked() {
-    isclicked === true ? setIsClicked(false) : setIsClicked(true);
-  }
+  useEffect( async () => {
+    statisticsService.getFavoritesCoursesByUser(4);
+    let favs = statisticsService.getFavoritesCoursesByUser(4).idTheme;
+    setFavThemes(favs)
+}, [])
 
-  return (
-    <Wrapper>
-      <PageTitle>Salvos</PageTitle>
-      <Stats>Acesse aqui seus conteúdos salvos</Stats>
-      <FilterContainer>
-        <ButtonS onClick={isClicked} isclicked={isclicked}>
-          Tudo
-        </ButtonS>
-        <ButtonS onClick={isClicked} isclicked={isclicked}>
-          Dev Full Stack
-        </ButtonS>
-        <ButtonS onClick={isClicked} isclicked={isclicked}>
-          UX/UI Design
-        </ButtonS>
-        <ButtonS onClick={isClicked} isclicked={isclicked}>
-          QA
-        </ButtonS>
-        <ButtonS onClick={isClicked} isclicked={isclicked}>
-          Soft Skills
-        </ButtonS>
-      </FilterContainer>
-      <ContainerTheme>
-        {roadDev.map((card, index) => (
-          <ChosedRoadCard
-            id={card.id}
-            title={card.title}
-            idType={card.idType}
-            time={card.time}
-            idTheme={card.idTheme}
-            idRoad={card.idRoad}
-            link={card.link}
-            key={index}
-          />
-        ))}
-      </ContainerTheme>
-    </Wrapper>
-  );
 }
