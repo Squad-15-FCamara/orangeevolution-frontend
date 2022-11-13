00535112da84699eac88b5a5f2ed1ede5d462e21
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-12 21:30:10 -0300

    refactor: all admin services

---
+++ src/services/adminServices.js
@@ -0,0 +1,36 @@
+import api from "./api";
+
+export const adminServices = {
+
+    // METODOS DO ADMINISTRADOR (buscar e criar trilhas, temas, tipos)
+    getAllRoadsName: async() => {
+        let response = api.get(`adminServices/roads/names`);
+        return response;
+    },
+
+    getAllThemesName: async() => {
+        let reponse = api.get(`adminServices/themes/names`);
+        return response;
+    },
+
+    getAllTypesName: async() => {
+        let response = api.get(`/adminServices/types/names`);
+        return response;
+    },
+
+    createANewRoad: async(road) => {
+        let response = api.post(`adminServices/roads`, road);
+        return response;
+    },
+
+    createANewTheme: async(theme) => {
+        let response = api.post(`adminServices/themes`, theme);
+        return response;
+    },
+
+    createANewType: async(type) => {
+        let response = api.post(`adminServices/types`, type);
+        return response;
+    }
+
+}
