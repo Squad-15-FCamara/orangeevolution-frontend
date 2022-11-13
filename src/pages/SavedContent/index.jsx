770ae2ef1e0cb4136f16645f98599a8d0e9bf41b
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-12 22:53:38 -0300

    feat: filter for favorite courses

--- src/pages/SavedContent/index.jsx
+++ src/pages/SavedContent/index.jsx
@@ -4,10 +4,19 @@ export function SavedContent() {
   const [filtered, setFiltered] = useState([]);
   const [favThemes, setFavThemes] = useState([]);
 
-  useEffect( async () => {
+  useEffect(async () => {
     statisticsService.getFavoritesCoursesByUser(4);
     let favs = statisticsService.getFavoritesCoursesByUser(4).idTheme;
     setFavThemes(favs)
-}, [])
+  }, [])
+
+  const fetchCourses = async (title) => {
+    const response = await statisticsService.getFavoritesCoursesByUser(4);
+    if (title === "Tudo") {
+      return setFiltered(response.data);
+    }
+    const filteredContent = response.data.filter(item => item.idTheme.toLowerCase().includes(title.toLowerCase()));
+    setFiltered(filteredContent);
+  }
 
 }
