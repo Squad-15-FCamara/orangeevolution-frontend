3e00fcf20e37c7e0ee4413d5454e6e04b1fa8490
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 03:59:30 -0300

    refactor: setup link to homepage on logo and fix link on home button. changes saved content icon

--- src/components/SideBarMenu/SideMenu.jsx
+++ src/components/SideBarMenu/SideMenu.jsx
@@ -1,6 +1,13 @@
-import { faArrowRightFromBracket, faChartLine, faBell, faFolderOpen, faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import avatar from './avatar.svg'
-import logo from './logo_orange.svg'
+import {
+  faArrowRightFromBracket,
+  faChartLine,
+  faBell,
+  faBookmark,
+  faHouse,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
+import avatar from './avatar.svg';
+import logo from './logo_orange.svg';
 import {
   Wrapper,
   Logo,
@@ -26,76 +33,83 @@ import {
   TextPastas,
   TextProgression,
   Modal,
-  InputDiv
-} from "./style";
+  InputDiv,
+} from './style';
 
-  import { Link, useLocation, useParams } from 'react-router-dom';
-import { useState } from "react";
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 export const SideMenu = () => {
+  const [input, setInput] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [input, setInput] = useState({})
-  const [isOpen, setIsOpen] = useState(false)
+  let { pathname } = useLocation();
 
-  let {pathname} = useLocation()
-
-
-  const UpdateInput = (e) =>{
+  const UpdateInput = (e) => {
     // const name = e.target.name;
     // const value = e.target.value;
     setInput({
       ...input,
       [name]: value,
-     })
+    });
 
     // console.log(input)
-
-  }
+  };
 
   // state={{data: input}}
 
-  return(
+  return (
     <>
-    <Wrapper>
-      <Logo src={logo}/>
-      <Menu>
+      <Wrapper>
+        <a href="/home">
+          <Logo src={logo} />
+        </a>
+        <Menu>
           <Links>
             <InputDiv>
-              <LinkStyledSearch to={`search/${input}`} >
+              <LinkStyledSearch to={`search/${input}`}>
                 <SearchIcon icon={faMagnifyingGlass} />
               </LinkStyledSearch>
-              <Input type='text' placeholder='Buscar' name='search' value={input.search} onChange={(e)=> setInput(e.target.value)}/>
+              <Input
+                type="text"
+                placeholder="Buscar"
+                name="search"
+                value={input.search}
+                onChange={(e) => setInput(e.target.value)}
+              />
             </InputDiv>
 
-
-            <HomeLinkStyled pathname={pathname} to='/'>
-              <HomeIcon pathname={pathname} icon={faHouse}/>
+            <HomeLinkStyled pathname={pathname} to="home">
+              <HomeIcon pathname={pathname} icon={faHouse} />
               <TextHome pathname={pathname}>Home</TextHome>
             </HomeLinkStyled>
-            <PastasLinkStyled pathname={pathname} to='/savedcontent'>
-              <PastasIcon pathname={pathname} icon={faFolderOpen}/>
+            <PastasLinkStyled pathname={pathname} to="/savedcontent">
+              <PastasIcon pathname={pathname} icon={faBookmark} />
               <TextPastas pathname={pathname}> Salvos </TextPastas>
             </PastasLinkStyled>
-            <NotificaLinkStyled pathname={pathname} to='/profile'>
+            <NotificaLinkStyled pathname={pathname} to="/profile">
               <NotificaIcon pathname={pathname} icon={faBell} />
               <TextNotifica pathname={pathname}>Notificações</TextNotifica>
             </NotificaLinkStyled>
-            <ProgressionLinkStyled pathname={pathname} to='/profile'>
-              <ProgressoIcon pathname={pathname} icon={faChartLine}/>
+            <ProgressionLinkStyled pathname={pathname} to="/profile">
+              <ProgressoIcon pathname={pathname} icon={faChartLine} />
               <TextProgression pathname={pathname}>Progresso</TextProgression>
             </ProgressionLinkStyled>
           </Links>
-        <Profile>
-            <Link to='/profile'><Avatar src={avatar}/></Link>
-            <Link to='/profile'><Name>Thiago Bandeira</Name></Link>
-            <Link onClick={()=> setIsOpen(true)}><LogOutIcon icon={faArrowRightFromBracket}/></Link>
-        </Profile>
-
-      </Menu>
-
-    </Wrapper>
+          <Profile>
+            <Link to="/profile">
+              <Avatar src={avatar} />
+            </Link>
+            <Link to="/profile">
+              <Name>Thiago Bandeira</Name>
+            </Link>
+            <Link onClick={() => setIsOpen(true)}>
+              <LogOutIcon icon={faArrowRightFromBracket} />
+            </Link>
+          </Profile>
+        </Menu>
+      </Wrapper>
       {isOpen && <Modal setIsOpen={setIsOpen} />}
-
     </>
-  )
-}
+  );
+};
