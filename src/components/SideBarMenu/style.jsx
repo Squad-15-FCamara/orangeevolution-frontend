f82278e0a50daaf8859e4b2519e14490b911e41c
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 11:45:49 -0300

    fix: set login page to new url on logout modal

--- src/components/SideBarMenu/style.jsx
+++ src/components/SideBarMenu/style.jsx
@@ -1,11 +1,15 @@
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTriangleExclamation, faCircleXmark, faCircleCheck} from "@fortawesome/free-solid-svg-icons";
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTriangleExclamation,
+  faCircleXmark,
+  faCircleCheck,
+} from '@fortawesome/free-solid-svg-icons';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Wrapper = styled.div`
-   width: 235px;
+  width: 235px;
   height: 100vh;
   position: fixed;
   background-color: white;
@@ -13,19 +17,18 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 export const Menu = styled.div`
   width: 235px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-
-`
+`;
 
 export const Logo = styled.img`
   margin-top: 20px;
-`
+`;
 
 export const Links = styled.div`
   display: flex;
@@ -33,8 +36,7 @@ export const Links = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 20px;
-`
-
+`;
 
 export const HomeLinkStyled = styled(Link)`
   display: flex;
@@ -46,24 +48,23 @@ export const HomeLinkStyled = styled(Link)`
   border-radius: 10px;
   margin-top: 16px;
   cursor: pointer;
-  background-color: ${(props)=> props.pathname === '/home' ? '#36357E': 'white'};
-`
+  background-color: ${(props) =>
+    props.pathname === '/home' ? '#36357E' : 'white'};
+`;
 
 export const TextHome = styled.p`
-  color:  ${(props)=> props.pathname === '/home' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/home' ? 'white' : '#36357E')};
   list-style-type: none;
   font-size: 20px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
-`
+`;
 
 export const HomeIcon = styled(FontAwesomeIcon)`
-  color:  ${(props)=> props.pathname === '/home' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/home' ? 'white' : '#36357E')};
   padding-left: 26px;
   font-size: 25px;
-`
-
-
+`;
 
 export const PastasLinkStyled = styled(Link)`
   display: flex;
@@ -75,24 +76,24 @@ export const PastasLinkStyled = styled(Link)`
   border-radius: 10px;
   margin-top: 16px;
   cursor: pointer;
-  background-color: ${(props)=> props.pathname === '/savedcontent' ? '#36357E': 'white'};
-`
+  background-color: ${(props) =>
+    props.pathname === '/savedcontent' ? '#36357E' : 'white'};
+`;
 export const PastasIcon = styled(FontAwesomeIcon)`
-  color: ${(props)=> props.pathname === '/savedcontent' ? 'white': '#36357E'};
+  color: ${(props) =>
+    props.pathname === '/savedcontent' ? 'white' : '#36357E'};
   padding-left: 29px;
   font-size: 25px;
-`
+`;
 
 export const TextPastas = styled.p`
-  color:  ${(props)=> props.pathname === '/savedcontent' ? 'white': '#36357E'};
+  color: ${(props) =>
+    props.pathname === '/savedcontent' ? 'white' : '#36357E'};
   list-style-type: none;
   font-size: 20px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
-`
-
-
-
+`;
 
 export const NotificaLinkStyled = styled(Link)`
   display: flex;
@@ -104,29 +105,25 @@ export const NotificaLinkStyled = styled(Link)`
   border-radius: 10px;
   margin-top: 16px;
   cursor: pointer;
-  background-color: ${(props)=> props.pathname === '/profile' ? '#36357E': 'white'};
-`
+  background-color: ${(props) =>
+    props.pathname === '/profile' ? '#36357E' : 'white'};
+`;
 export const NotificaIcon = styled(FontAwesomeIcon)`
-  color: ${(props)=> props.pathname === '/profile' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/profile' ? 'white' : '#36357E')};
   padding-left: 29px;
   font-size: 25px;
-`
+`;
 
 export const TextNotifica = styled.p`
-  color:  ${(props)=> props.pathname === '/profile' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/profile' ? 'white' : '#36357E')};
   list-style-type: none;
   font-size: 20px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
-
-
-`
-
-
-
+`;
 
 export const ProgressionLinkStyled = styled(Link)`
-   display: flex;
+  display: flex;
   height: 48px;
   align-items: center;
   text-decoration: none;
@@ -135,32 +132,32 @@ export const ProgressionLinkStyled = styled(Link)`
   border-radius: 10px;
   margin-top: 16px;
   cursor: pointer;
-  background-color: ${(props)=> props.pathname === '/profile' ? '#36357E': 'white'};
-`
+  background-color: ${(props) =>
+    props.pathname === '/profile' ? '#36357E' : 'white'};
+`;
 export const ProgressoIcon = styled(FontAwesomeIcon)`
-  color: ${(props)=> props.pathname === '/profile' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/profile' ? 'white' : '#36357E')};
   padding-left: 29px;
   font-size: 25px;
-`
+`;
 
 export const TextProgression = styled.p`
-  color:  ${(props)=> props.pathname === '/profile' ? 'white': '#36357E'};
+  color: ${(props) => (props.pathname === '/profile' ? 'white' : '#36357E')};
   list-style-type: none;
   font-size: 20px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
-`
+`;
 
 export const InputDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid #36357E;
+  border: 1px solid #36357e;
   height: 48px;
   border-radius: 10px;
   width: 220px;
-`
-
+`;
 
 export const LinkStyled = styled(Link)`
   display: flex;
@@ -172,15 +169,15 @@ export const LinkStyled = styled(Link)`
   border-radius: 10px;
   margin-top: 16px;
   cursor: pointer;
-  background-color: ${(props)=> props.pathname === '/' ? '#36357E': 'white'};
-
-`
+  background-color: ${(props) =>
+    props.pathname === '/' ? '#36357E' : 'white'};
+`;
 export const LinkStyledSearch = styled(LinkStyled)`
   width: 20px;
   height: 40px;
   position: relative;
   margin: 0;
-`
+`;
 export const Input = styled.input`
   height: 25px;
   outline: none;
@@ -193,66 +190,55 @@ export const Input = styled.input`
   font-family: 'Montserrat';
   background-color: transparent;
 
-  ::placeholder{
-    color: #36357E;
+  ::placeholder {
+    color: #36357e;
     font-weight: 400;
   }
 
   :hover {
-
-    ::placeholder{
+    ::placeholder {
       color: white;
     }
   }
-`
+`;
 
 export const SearchIcon = styled(FontAwesomeIcon)`
-    color: #36357E;
-    padding-right: 15px;
-    font-size: 25px;
-    position: absolute;
-    bottom: 8px;
-`
+  color: #36357e;
+  padding-right: 15px;
+  font-size: 25px;
+  position: absolute;
+  bottom: 8px;
+`;
 
 export const LogOutIcon = styled(FontAwesomeIcon)`
   color: #ffffff;
   transform: rotate(180deg);
   font-size: 25px;
-`
+`;
 
 export const Profile = styled.div`
   display: flex;
   align-items: center;
-  background-color: #36357E;
+  background-color: #36357e;
   gap: 10px;
   height: 80px;
   border-radius: 10px 10px 0px 0px;
   position: absolute;
   bottom: 0;
   width: 235px;
-`
+`;
 
 export const Avatar = styled.img`
-   max-width: 40px;
-   padding-left: 16px;
-`
-
+  max-width: 40px;
+  padding-left: 16px;
+`;
 
 export const Name = styled.p`
-  color: #FBFBFB;
+  color: #fbfbfb;
   font-size: 14px;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
-`
-
-
-
-
-
-
-
-
-
+`;
 
 //Modal de logout
 
@@ -271,7 +257,7 @@ export const WrapperModal = styled.div`
   align-items: center;
   justify-content: center;
   transition: 2s all;
-`
+`;
 
 export const WrapperContent = styled.div`
   width: 500px;
@@ -282,37 +268,36 @@ export const WrapperContent = styled.div`
   justify-content: center;
   background-color: white;
   gap: 40px;
-`
+`;
 
-export const WarningDiv= styled.div`
+export const WarningDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
   gap: 15px;
   margin-top: 10px;
-
-`
+`;
 
 export const WarningIcon = styled(FontAwesomeIcon)`
   color: red;
-`
+`;
 
 export const WarningText = styled.h3`
   font-size: 28px;
   font-weight: 400;
   margin: 0;
-`
+`;
 
 export const Buttons = styled.div`
   display: flex;
   justify-content: center;
   gap: 50px;
   width: 100%;
-`
+`;
 
 export const LogoutBtn = styled.a`
   height: 48px;
-  background-color: #FF4848;
+  background-color: #ff4848;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -322,11 +307,10 @@ export const LogoutBtn = styled.a`
   border-radius: 10px;
   color: white;
   cursor: pointer;
-
-`
+`;
 export const LogOutIconModal = styled(FontAwesomeIcon)`
   color: white;
-`
+`;
 
 export const KeepInBtn = styled.button`
   height: 48px;
@@ -337,38 +321,37 @@ export const KeepInBtn = styled.button`
   justify-content: center;
   gap: 10px;
   cursor: pointer;
-  border: 1px solid #36357E;
-`
+  border: 1px solid #36357e;
+`;
 
 export const KeepInIcon = styled(FontAwesomeIcon)`
-  color: #36357E;
-`
+  color: #36357e;
+`;
 
 export const KeepInText = styled.h3`
-  color: #36357E;
+  color: #36357e;
   font-size: 14px;
-`
+`;
 
-export const Modal = ({setIsOpen}) =>{
-  return(
-    <WrapperModal onClick={()=> setIsOpen(false)}>
+export const Modal = ({ setIsOpen }) => {
+  return (
+    <WrapperModal onClick={() => setIsOpen(false)}>
       <WrapperContent>
         <WarningDiv>
-          <WarningIcon className='fa-2xl' icon={faTriangleExclamation}/>
+          <WarningIcon className="fa-2xl" icon={faTriangleExclamation} />
           <WarningText>Deseja sair?</WarningText>
         </WarningDiv>
         <Buttons>
-          <LogoutBtn href='/login'>
-            <LogOutIconModal className='fa-xl' icon={faCircleXmark}/>
+          <LogoutBtn href="/">
+            <LogOutIconModal className="fa-xl" icon={faCircleXmark} />
             Quero sair
           </LogoutBtn>
           <KeepInBtn>
-            <KeepInIcon className='fa-xl' icon={faCircleCheck}/>
+            <KeepInIcon className="fa-xl" icon={faCircleCheck} />
             <KeepInText> Continuar logado </KeepInText>
           </KeepInBtn>
         </Buttons>
       </WrapperContent>
     </WrapperModal>
-
-  )
-}
+  );
+};
