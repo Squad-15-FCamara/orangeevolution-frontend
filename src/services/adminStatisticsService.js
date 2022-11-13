d9b699d7e764608f5c77522b2fb21ccdbd0ab4b6
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-12 21:30:26 -0300

    refactor: admin statistics methods

---
+++ src/services/adminStatisticsService.js
@@ -0,0 +1,31 @@
+import api from "./api";
+
+export const adminStatistics = {
+
+    //ESTATÍSTICAS DO ADMINISTRADOR (número de cursos, usuários, cursos em andamento, cursos concluídos e cursos não-feitos)
+    getNumberOfCourses: async () => {
+        let response = api.get(`statisticAdmin/counterCourses`);
+        return response;
+    },
+
+    getNumberOfUsers: async () => {
+        let response = api.get(`statisticAdmin/counterUser`);
+        return response;
+    },
+
+    usersNotDoingACourse: async(courseId) => {
+        let response = api.get(`statisticAdmin/coursesDidnt/${courseId}`);
+        return response;
+    },
+
+    usersDoingACourse: async(courseId) => {
+        let response = api.get(`statisticAdmin/coursesDoing/${courseId}`);
+        return response;
+    },
+
+    userDoneACourse: async(courseId) => {
+        let response = api.get(`statisticAdmin/coursesDone/${courseId}`);
+        return response;
+    }
+
+}
