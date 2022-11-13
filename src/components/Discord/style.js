faedd9d0c109bb1a69779b52b82a19b1d6f14bd7
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 13:35:08 -0300

    feat: creates discord icon component

---
+++ src/components/Discord/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DiscordContainer = styled.div`
+  width: 85px;
+  height: 85px;
+  position: fixed;
+  right: 60px;
+  bottom: 50px;
+  z-index: 10;
+  &:hover {
+    scale: 1.1;
+  }
+`;
