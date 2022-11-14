600eef28a0b6e93e93a52820267b038891234909
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-14 14:45:02 -0300

    feat: config react media to render menus according to window width

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,4 +1,6 @@
+import Media from 'react-media';
 import { Outlet } from 'react-router-dom';
+import { BottomMobileMenu } from '../components/BottomMobileMenu';
 import { DiscordIcon } from '../components/Discord';
 // import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
@@ -8,10 +10,15 @@ import { SideMenu } from '../components/SideBarMenu/SideMenu';
 export function DefaultLayout() {
   return (
     <div>
-      <SideMenu />
+      <Media query="(max-width: 900px)" render={() => <BottomMobileMenu />} />
+
+      <Media query="(min-width: 900px)" render={() => <SideMenu />} />
+
+      <Media query="(min-width: 900px)" render={() => <DiscordIcon />} />
+
       <Outlet />
-      <DiscordIcon />
-      <Footer />
+
+      <Media query="(min-width: 900px)" render={() => <Footer />} />
     </div>
   );
 }
