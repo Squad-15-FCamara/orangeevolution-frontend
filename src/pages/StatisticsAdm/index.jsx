e1220e5ae39bc444e6a9fa335fc48317c2893801
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-14 01:59:23 -0300

    feat: creates adm statistics page

--- src/pages/StatisticsAdm/index.jsx
+++ src/pages/StatisticsAdm/index.jsx
@@ -1,9 +1,48 @@
 import { Wrapper } from '../Home/style';
+import { ContainerTheme, LinksContainer, Stats } from '../RoadDev/style';
+import { PageTitle } from '../SavedContent/style';
+import { InfoContainer } from './style';
+import { useEffect, useState } from 'react';
+import { adminService } from '../../services/adminService';
+import { RoadStatistics } from '../../components/RoadStatistics';
 
 export function StatisticsAdm() {
+  const [getTheme, setGetTheme] = useState([]);
+
+  useEffect(() => {
+    fetchThemes();
+  }, []);
+
+  const fetchThemes = async () => {
+    try {
+      let response = await adminService.getAllThemesName();
+      setGetTheme(response.data);
+    } catch (e) {
+      console.error('Ops! Encontramos um erro: ' + e);
+    }
+  };
+
   return (
     <Wrapper>
-      <h1>Teste</h1>
+      <LinksContainer>
+        <a href="#">Trilhas</a>
+      </LinksContainer>
+      <PageTitle>Estatísticas</PageTitle>
+      <Stats>Confira aqui as estatísticas de acesso</Stats>
+      <InfoContainer>
+        <h2>Trilha</h2>
+        <span>|</span>
+        <h2>Acessos</h2>
+        <span>|</span>
+        <h2>Concluídos</h2>
+        <span>|</span>
+        <h2>Não Acessado</h2>
+      </InfoContainer>
+      <ContainerTheme>
+        {getTheme.map((theme, index) => (
+          <RoadStatistics theme={theme} key={index} />
+        ))}
+      </ContainerTheme>
     </Wrapper>
   );
 }
