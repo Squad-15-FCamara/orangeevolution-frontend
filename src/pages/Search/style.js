25918d5e3ef766fcd2d52b7d5cf58d05aeb282d7
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-14 15:00:57 -0300

    fix: add media query to adjust in mobile screen

--- src/pages/Search/style.js
+++ src/pages/Search/style.js
@@ -1,15 +1,18 @@
-import styled from "styled-components"
-
-
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   margin-left: 300px;
   padding-top: 50px;
   padding-bottom: 100px;
-`
+
+  @media screen and (max-width: 900px) {
+    padding-left: 30px;
+    margin-left: 0;
+  }
+`;
 
 export const Title = styled.h1`
   padding-bottom: 50px;
   font-weight: 800;
   font-size: 40px;
-`
+`;
