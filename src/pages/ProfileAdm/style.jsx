b82e7812e4c52dd06b2259d8690f9267bad24873
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 21:00:26 -0300

    feat: creates adm profile

---
+++ src/pages/ProfileAdm/style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MyProfile = styled.h1`
+  margin: 54px 0 39px 0;
+  color: #333333;
+`;
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: flex-end;
+  h2 {
+    margin: 0;
+    font-size: 1.75rem;
+    color: #333333;
+  }
+`;
+
+export const Bio = styled.p`
+  width: 75%;
+  margin-top: 36px;
+`;
