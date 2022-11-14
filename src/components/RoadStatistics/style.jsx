cc5c3c1aa9333a29b328ace785691414255df7cf
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-14 15:10:05 -0300

    refactor: changes props names to match api

---
+++ src/components/RoadStatistics/style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Theme = styled.h2`
+  font-weight: 600;
+  font-size: 1.25rem;
+  padding-left: 21px;
+  width: 100px;
+  text-align: center;
+`;
