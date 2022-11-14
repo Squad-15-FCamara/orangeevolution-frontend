e0ef5f83959170ee38ecf1f80c374a5c98054b09
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-13 21:16:19 -0300

    feat: target blank at discord icon

--- src/components/Discord/index.jsx
+++ src/components/Discord/index.jsx
@@ -4,7 +4,7 @@ import { DiscordContainer } from './style';
 export function DiscordIcon() {
   return (
     <DiscordContainer>
-      <a href="https://discord.com/invite/NtESsDFGx5">
+      <a href="https://discord.com/invite/NtESsDFGx5" target="blank">
         <img src={Discord} />
       </a>
     </DiscordContainer>
