87d74d5e2d02826a883af5243f44e580f1337b05
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-14 15:06:13 -0300

    feat: adds statistics services

--- src/services/adminStatisticsService.js
+++ src/services/adminStatisticsService.js
@@ -1,61 +1,85 @@
-import api from "./api";
+import api from './api';
 
 export const adminStatistic = {
+  //ESTATÍSTICAS DO ADMINISTRADOR (número de cursos, usuários, cursos em andamento, cursos concluídos e cursos não-feitos)
+  getNumberOfUsers: async () => {
+    let response = api.get(`statisticAdmin/counterUser`);
+    return response;
+  },
 
-    //ESTATÍSTICAS DO ADMINISTRADOR (número de cursos, usuários, cursos em andamento, cursos concluídos e cursos não-feitos)
-    getNumberOfUsers: async () => {
-        let response = api.get(`statisticAdmin/counterUser`);
-        return response;
-    },
+  getNumberOfCourses: async () => {
+    let response = api.get(`statisticAdmin/counterCourses`);
+    return response;
+  },
 
-    getNumberOfCourses: async () => {
-        let response = api.get(`statisticAdmin/counterCourses`);
-        return response;
-    },
+  usersDoingACourse: async (courseId) => {
+    let response = api.get(`statisticAdmin/coursesDoing/${courseId}`);
+    return response;
+  },
 
-    usersDoingACourse: async(courseId) => {
-        let response = api.get(`statisticAdmin/coursesDoing/${courseId}`);
-        return response;
-    },
+  usersNotDoingACourse: async (courseId) => {
+    let response = api.get(`statisticAdmin/coursesDidnt/${courseId}`);
+    return response;
+  },
 
-    usersNotDoingACourse: async(courseId) => {
-        let response = api.get(`statisticAdmin/coursesDidnt/${courseId}`);
-        return response;
-    },
+  userDoneACourse: async (courseId) => {
+    let response = api.get(`statisticAdmin/coursesDone/${courseId}`);
+    return response;
+  },
 
-    userDoneACourse: async(courseId) => {
-        let response = api.get(`statisticAdmin/coursesDone/${courseId}`);
-        return response;
-    },
+  usersDoingARoad: async (roadName) => {
+    let response = api.get(
+      `statisticAdmin/counter/user/doing/road/${roadName}`,
+    );
+    return response;
+  },
 
-    usersDoingARoad: async(roadName) => {
-        let response = api.get(`statisticAdmin/counter/user/doing/road/${roadName}`);
-        return response;
-    },
+  usersDidntARoad: async (roadName) => {
+    let response = api.get(
+      `statisticAdmin/counter/user/didnt/road/${roadName}`,
+    );
+    return response;
+  },
 
-    usersDidntARoad: async(roadName) => {
-        let response = api.get(`statisticAdmin/counter/user/didnt/road/${roadName}`);
-        return response;
-    },
+  userDoneARoad: async (roadName) => {
+    let response = api.get(
+      `/statisticAdmin/counter/user/done/road/${roadName}`,
+    );
+    return response;
+  },
 
-    userDoneARoad: async(roadName) => {
-        let response = api.get(`/statisticAdmin/counter/user/done/road/${roadName}`);
-        return response;
-    },
+  usersDoingATheme: async (themeName) => {
+    let response = api.get(
+      `/statisticAdmin/counter/user/doing/theme/${themeName}`,
+    );
+    return response;
+  },
 
-    usersDoingATheme: async(themeName) => {
-        let response = api.get(`/statisticAdmin/counter/user/doing/theme/${themeName}`);
-        return response;
-    },
+  usersDidntATheme: async (themeName) => {
+    let response = api.get(
+      `statisticAdmin/counter/user/didnt/theme/${themeName}`,
+    );
+    return response;
+  },
 
-    usersDidntATheme: async(themeName) => {
-        let response = api.get(`statisticAdmin/counter/user/didnt/theme/${themeName}`);
-        return response;
-    },
+  usersDoneATheme: async (themeName) => {
+    let response = api.get(
+      `/statisticAdmin/counter/user/done/theme/${themeName}`,
+    );
+    return response;
+  },
 
-    usersDoneATheme: async(themeName) => {
-        let response = api.get(`/statisticAdmin/counter/user/done/theme/${themeName}`);
-        return response;
-    }
+  usersStatsByRoad: async (roadName) => {
+    let response = api.get(
+      `/statisticAdmin/counter/statitcs/roads/${roadName}`,
+    );
+    return response;
+  },
 
-}
+  usersStatsByTheme: async (themeName) => {
+    let response = api.get(
+      `/statisticAdmin/counter/statistic/themes/${themeName}`,
+    );
+    return response;
+  },
+};
