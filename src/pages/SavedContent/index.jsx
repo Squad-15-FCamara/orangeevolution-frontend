9c7fb9a3f7d0d5c82c25e74ce9fa2d4128645c8c
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-15 19:58:28 -0300

    refactor: using context instead of http request in favorites page

--- src/pages/SavedContent/index.jsx
+++ src/pages/SavedContent/index.jsx
@@ -4,46 +4,34 @@ import { ContainerTheme } from '../ContentIntro/style';
 import { Wrapper } from '../Home/style';
 import { Stats } from '../RoadDev/style';
 import { ButtonS, FilterContainer, PageTitle } from './style';
-import { statisticsService } from '../../services/statisticsService';
-import { adminService } from '../../services/adminService';
 import { ALL_ROADS } from './roads';
 import { favContext } from '../../context/favoritesContext';
 import { Loading } from '../../components/Loading';
 
 export function SavedContent() {
   const [favCourses, setFavCourses] = useState([]);
-  const [allFavCourses, setAllFavCourses] = useState([]);
-  const [allRoads, setAllRoads] = useState([]);
   const [clicked, setClicked] = useState('Tudo');
   const { favorites } = useContext(favContext);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetchInitialCoursesState();
+    setIsLoading(true);
+    setFavCourses(favorites);
+    setIsLoading(false);
   }, []);
 
-  const fetchInitialCoursesState = async () => {
-    try {
-      let response = await statisticsService.getFavoriteCoursesByUser(4);
-      setAllFavCourses(response.data);
-      setFavCourses(response.data);
-      setIsLoading(false);
-    } catch (e) {
-      console.error('Ops! Encontramos um erro: ' + e);
-    }
-  };
-
   const handleClick = async (title) => {
-    setClicked(title);
-    if (!favCourses) return console.error('não há cursos favoritos!');
-    if (title === 'Tudo') {
-      return setFavCourses(allFavCourses);
+    setIsLoading(true)
+    setClicked(title)
+    if (!favorites) return console.error('não há cursos favoritos!');
+    if (title === "Tudo") {
+      setIsLoading(false)
+      return setFavCourses(favorites);
     }
-    const filteredContent = allFavCourses.filter((item) =>
-      item.idRoad.includes(title),
-    );
+    const filteredContent = favorites.filter(item => item.idRoad.includes(title));
     setFavCourses(filteredContent);
-  };
+    setIsLoading(false);
+  }
 
   return (
     <Wrapper>
@@ -74,13 +62,7 @@ export function SavedContent() {
               idRoad={card.idRoad}
               link={card.link}
               key={card.id}
-              arrFav={
-                favorites
-                  ? favorites
-                  : console.log(
-                      'tem nada aqui nao chefe kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
-                    )
-              }
+              arrFav={favorites}
             />
           ))
         ) : (
