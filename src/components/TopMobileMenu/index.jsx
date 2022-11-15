948fe7e198e144c9e752a916011f4ec324e90417
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-15 02:59:03 -0300

    style: add search to top mobile menu

--- src/components/TopMobileMenu/index.jsx
+++ src/components/TopMobileMenu/index.jsx
@@ -1,17 +1,42 @@
-import { Wrapper, LogoDiv, Notification, LogOut, Logo } from './style';
+import {
+  Wrapper,
+  LogoDiv,
+  Notification,
+  LogOut,
+  Logo,
+  InputDivMobile,
+  SearchIconMobile,
+} from './style';
 import {
   faArrowRightFromBracket,
   faBell,
+  faMagnifyingGlass,
 } from '@fortawesome/free-solid-svg-icons';
 import LogoMobile from './logo_mobile.svg';
 import { Link } from 'react-router-dom';
+import { LinkStyledSearch, SearchIcon, Input } from '../SideBarMenu/style';
+import { useContext } from 'react';
+import { Context } from '../../context/context';
 
 export const TopMobileMenu = () => {
+  const { inputSearch, setInputSearch } = useContext(Context);
+
   return (
     <Wrapper>
       <LogoDiv to="/home">
         <Logo src={LogoMobile} />
       </LogoDiv>
+      <InputDivMobile>
+        <LinkStyledSearch to="search">
+          <SearchIconMobile icon={faMagnifyingGlass} />
+        </LinkStyledSearch>
+        <Input
+          type="text"
+          placeholder="Buscar"
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+        />
+      </InputDivMobile>
       <Notification icon={faBell} />
       <Link to="/">
         <LogOut icon={faArrowRightFromBracket} />
