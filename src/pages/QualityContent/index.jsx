14ba297dd1c9ce02fded89341f3835065e464f3d
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-14 23:01:00 -0300

    feat: creates qa theme pages

---
+++ src/pages/QualityContent/index.jsx
@@ -0,0 +1,71 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+
+import { Wrapper } from '../Home/style';
+import { Gradient, LinksContainer, Stats, Themes } from '../ContentIntro/style';
+import { ChosedRoadCard } from '../../components/ChosedRoadCard';
+import { ContainerTheme } from '../RoadDev/style';
+import { adminStatistic } from '../../services/adminStatisticsService';
+
+export function QualityContent() {
+  const [roadQualidade, setRoadQualidade] = useState([]);
+  const [themeCounter, setThemeCounter] = useState([]);
+
+  useEffect(() => {
+    getCourses();
+    fetchCounterTheme();
+  }, []);
+
+  async function getCourses() {
+    await api
+      .get('/courses/themes/Qualidade')
+      .then((response) => setRoadQualidade(response.data))
+      .catch((err) => {
+        console.error('ops! ocorreu um erro' + err);
+      });
+    [];
+  }
+
+  const fetchCounterTheme = async () => {
+    try {
+      let response = await adminStatistic.usersDoingATheme('Qualidade');
+      setThemeCounter(response.data);
+    } catch (e) {
+      console.error('Ops! Encontramos um erro: ' + e);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <LinksContainer>
+        <a href="home">Home</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="/roadqa">Quality Assurance QA</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="#">Qualidade</a>
+      </LinksContainer>
+      <Gradient>Qualidade</Gradient>
+      <Stats>{themeCounter} pessoas estão estudando essa trilha</Stats>
+      <Themes>Conteúdos - 00/30</Themes>
+      <ContainerTheme>
+        {roadQualidade.map((card, index) => (
+          <ChosedRoadCard
+            id={card.id}
+            title={card.title}
+            idType={card.idType}
+            time={card.time}
+            idTheme={card.idTheme}
+            idRoad={card.idRoad}
+            link={card.link}
+            key={index}
+            description={card.description}
+            author={card.author}
+          />
+        ))}
+      </ContainerTheme>
+    </Wrapper>
+  );
+}
---
+++ src/pages/TestesContent/index.jsx
@@ -0,0 +1,73 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+
+import { Wrapper } from '../Home/style';
+import { Gradient, LinksContainer, Stats, Themes } from '../ContentIntro/style';
+import { ChosedRoadCard } from '../../components/ChosedRoadCard';
+import { ContainerTheme } from '../RoadDev/style';
+import { adminStatistic } from '../../services/adminStatisticsService';
+
+export function TestesContent() {
+  const [roadTestes, setRoadTeste] = useState([]);
+  const [themeCounter, setThemeCounter] = useState([]);
+
+  useEffect(() => {
+    getCourses();
+    fetchCounterTheme();
+  }, []);
+
+  async function getCourses() {
+    await api
+      .get('/courses/themes/Testes%20Automatizados')
+      .then((response) => setRoadTeste(response.data))
+      .catch((err) => {
+        console.error('ops! ocorreu um erro' + err);
+      });
+    [];
+  }
+
+  const fetchCounterTheme = async () => {
+    try {
+      let response = await adminStatistic.usersDoingATheme(
+        'Testes Automatizados',
+      );
+      setThemeCounter(response.data);
+    } catch (e) {
+      console.error('Ops! Encontramos um erro: ' + e);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <LinksContainer>
+        <a href="home">Home</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="/roadqa">Quality Assurance QA</a>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <a href="#">Testes Automatizados</a>
+      </LinksContainer>
+      <Gradient>Testes Automatizados</Gradient>
+      <Stats>{themeCounter} pessoas estão estudando essa trilha</Stats>
+      <Themes>Conteúdos - 00/30</Themes>
+      <ContainerTheme>
+        {roadTestes.map((card, index) => (
+          <ChosedRoadCard
+            id={card.id}
+            title={card.title}
+            idType={card.idType}
+            time={card.time}
+            idTheme={card.idTheme}
+            idRoad={card.idRoad}
+            link={card.link}
+            key={index}
+            description={card.description}
+            author={card.author}
+          />
+        ))}
+      </ContainerTheme>
+    </Wrapper>
+  );
+}
