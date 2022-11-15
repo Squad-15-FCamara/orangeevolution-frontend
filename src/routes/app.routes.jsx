67fd51e6f3436e379fd865ca0e7b886d14d5cb5f
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-15 14:14:02 -0300

    feat: import favoriteContextProvider in routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -33,47 +33,50 @@ import { BddContent } from '../pages/BddContent';
 import { QualityContent } from '../pages/QualityContent';
 import { PersonalDevContent } from '../pages/PersonalDevContent';
 import { TeamWorkContent } from '../pages/TeamWorkContent';
+import { FavContextProvider } from '../context/favoritesContext';
 
 export function AppRoutes() {
   return (
     <ContextProvider>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/" element={<DefaultLayout />}>
-          <Route path="home" element={<Home />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="roaddev" element={<RoadDev />} />
-          <Route path="roadux" element={<RoadUx />} />
-          <Route path="roadqa" element={<RoadQa />} />
-          <Route path="roadsoft" element={<RoadSoft />} />
-          <Route path="contentintro" element={<ContentIntro />} />
-          <Route path="contentfull" element={<ContentFull />} />
-          <Route path="savedcontent" element={<SavedContent />} />
-          <Route path="progress" element={<Progress />} />
-          <Route path="search" element={<Search />} />
-          <Route path="uxcontent" element={<UxContent />} />
-          <Route path="uicontent" element={<UiContent />} />
-          <Route path="optionaluxcontent" element={<OptionalContent />} />
-          <Route path="testescontent" element={<TestesContent />} />
-          <Route path="bddcontent" element={<BddContent />} />
-          <Route path="qualitycontent" element={<QualityContent />} />
-          <Route path="personalcontent" element={<PersonalDevContent />} />
-          <Route path="teamcontent" element={<TeamWorkContent />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-        <Route path="/" element={<DefaultAdm />}>
-          <Route path="/homeadm" element={<HomeAdm />} />
-          <Route path="/roaddevadm" element={<RoadDevAdm />} />
-          <Route path="/roaduxadm" element={<RoadUxAdm />} />
-          <Route path="/roadqaadm" element={<RoadQaAdm />} />
-          <Route path="/roadsoftadm" element={<RoadSoftAdm />} />
-          <Route path="/statisticsadm" element={<StatisticsAdm />} />
-          <Route path="/profileadm" element={<ProfileAdm />} />
-          <Route path="/searchadm" element={<SearchAdm />} />
-          <Route path="/0" element={<IntroStatistics />} />
-          <Route path="/1" element={<FullStatistics />} />
-        </Route>
-      </Routes>
+      <FavContextProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="roaddev" element={<RoadDev />} />
+            <Route path="roadux" element={<RoadUx />} />
+            <Route path="roadqa" element={<RoadQa />} />
+            <Route path="roadsoft" element={<RoadSoft />} />
+            <Route path="contentintro" element={<ContentIntro />} />
+            <Route path="contentfull" element={<ContentFull />} />
+            <Route path="savedcontent" element={<SavedContent />} />
+            <Route path="progress" element={<Progress />} />
+            <Route path="search" element={<Search />} />
+            <Route path="uxcontent" element={<UxContent />} />
+            <Route path="uicontent" element={<UiContent />} />
+            <Route path="optionaluxcontent" element={<OptionalContent />} />
+            <Route path="testescontent" element={<TestesContent />} />
+            <Route path="bddcontent" element={<BddContent />} />
+            <Route path="qualitycontent" element={<QualityContent />} />
+            <Route path="personalcontent" element={<PersonalDevContent />} />
+            <Route path="teamcontent" element={<TeamWorkContent />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+          <Route path="/" element={<DefaultAdm />}>
+            <Route path="/homeadm" element={<HomeAdm />} />
+            <Route path="/roaddevadm" element={<RoadDevAdm />} />
+            <Route path="/roaduxadm" element={<RoadUxAdm />} />
+            <Route path="/roadqaadm" element={<RoadQaAdm />} />
+            <Route path="/roadsoftadm" element={<RoadSoftAdm />} />
+            <Route path="/statisticsadm" element={<StatisticsAdm />} />
+            <Route path="/profileadm" element={<ProfileAdm />} />
+            <Route path="/searchadm" element={<SearchAdm />} />
+            <Route path="/0" element={<IntroStatistics />} />
+            <Route path="/1" element={<FullStatistics />} />
+          </Route>
+        </Routes>
+      </FavContextProvider>
     </ContextProvider>
   );
 }
