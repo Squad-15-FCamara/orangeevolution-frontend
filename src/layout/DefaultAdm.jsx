c4bc8f9aba8d3f1fd7db74cfd5ef66069f8ef4a1
Vitor Matias <vitormatiasdev@gmail.com>
2022-11-15 13:49:38 -0300

    feat: set media query on adm default layout

--- src/layout/DefaultAdm.jsx
+++ src/layout/DefaultAdm.jsx
@@ -1,15 +1,23 @@
+import Media from 'react-media';
 import { Outlet } from 'react-router-dom';
+import { BottomMobileMenuAdm } from '../components/BottomMobileMenuAdm';
 import { DiscordIcon } from '../components/Discord';
 import { Footer } from '../components/Footer';
 import { SideBarAdm } from '../components/SideBarAdm';
+import { TopMobileMenuAdm } from '../components/TopMobileMenuAdm';
 
 export function DefaultAdm() {
   return (
-    <div>
-      <SideBarAdm />
+    <>
+      <Media query="(min-width: 900px)" render={() => <SideBarAdm />} />
+      <Media query="(max-width: 900px)" render={() => <TopMobileMenuAdm />} />
+      <Media query="(min-width: 900px)" render={() => <DiscordIcon />} />
       <Outlet />
-      <DiscordIcon />
-      <Footer />
-    </div>
+      <Media
+        query="(max-width: 900px)"
+        render={() => <BottomMobileMenuAdm />}
+      />
+      <Media query="(min-width: 900px)" render={() => <Footer />} />
+    </>
   );
 }
