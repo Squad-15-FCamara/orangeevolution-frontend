35b980641a90e39e290acc44eec4b291c33e90d9
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-15 19:20:19 -0300

    fix: bug course desfavoriting yourself

--- src/components/ChosedRoadCard/index.jsx
+++ src/components/ChosedRoadCard/index.jsx
@@ -23,6 +23,8 @@ import {
 import { statisticsService } from '../../services/statisticsService';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useContext } from 'react';
+import { favContext } from '../../context/favoritesContext';
 
 export function ChosedRoadCard({
   id,
@@ -36,30 +38,33 @@ export function ChosedRoadCard({
   description,
   arrFav
 }) {
-  const [favorites, setFavorites] = useState([]);
+  const [favs, setFavs] = useState([]);
   const [isFavorite, setIsFavorite] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const { favorites, setFavorites } = useContext(favContext);
 
   useEffect(() => {
-    setFavorites(arrFav);
+    setFavs(arrFav);
   }, []);
 
-  let favExists;
-
   useEffect(() => {
-    favExists = favorites.some((fav) => fav.id == `${id}`);
+    let favExists = favs.some((fav) => fav.id == `${id}`);
     setIsFavorite(favExists);
-  }, [favorites]);
+  }, [favs]);
 
-  let handleFav = () => {};
-
-  const CheckFavorite = async (id, isFav) => {
+  const toggleFavorite = async (id, isFav) => {
     if (!isFav) {
-      handleFav = statisticsService.addAFavoriteCourse;
+      let response = await statisticsService.addAFavoriteCourse(4, id);
+      setFavorites((oldState) => {
+        return [...oldState, response.data]
+      })
+      console.log('favoritos>>>', favorites)
     } else {
-      handleFav = statisticsService.deleteFavoriteCourse;
+      await statisticsService.deleteFavoriteCourse(4, id);
+      setFavorites((oldState) => {
+        return oldState.filter(card => card.id !== id)
+      })
     }
-    await handleFav(4, `${id}`);
     setIsFavorite(!isFav);
   };
 
@@ -71,7 +76,7 @@ export function ChosedRoadCard({
           <button
             onClick={
               () =>
-                CheckFavorite(
+                toggleFavorite(
                   id,
                   isFavorite,
                 )
